1de0764f8dab49d8f19e5be300a424cc45de6347
frankjmclarke <frankjmclarke@gmail.com>
2023-06-04 16:37:55 -0700

    Add delete to Category List

--- lib/ui/category_list_ui.dart
+++ lib/ui/category_list_ui.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_starter/ui/stored_list_ui.dart';
 import 'package:flutter_starter/ui/url_list_ui.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
@@ -8,6 +7,30 @@ import '../models/category_model.dart';
 
 class CategoryListUI extends StatelessWidget {
   final CategoryController categoryController = Get.put(CategoryController());
+
+  void _showDeleteConfirmationDialog(CategoryModel category) {
+    Get.dialog(//no context required
+      AlertDialog(
+        title: Text('Delete Category'),
+        content: Text('Are you sure you want to delete this category?'),
+        actions: [
+          TextButton(
+            child: Text('Cancel'),
+            onPressed: () {
+              Get.back();
+            },
+          ),
+          TextButton(
+            child: Text('Delete'),
+            onPressed: () {
+              categoryController.deleteCategory(category);
+              Get.back();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,92 +97,67 @@ class CategoryListUI extends StatelessWidget {
         ? catModel.imageUrl
         : 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Feral_pigeon_%28Columba_livia_domestica%29%2C_2017-05-27.jpg/1024px-Feral_pigeon_%28Columba_livia_domestica%29%2C_2017-05-27.jpg';
 
-    return Dismissible(
-      key: Key(catModel.uid),
-      background: Container(
-        color: Colors.red,
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.only(right: 16.0),
-        child: Icon(
-          Icons.delete,
-          color: Colors.white,
-        ),
-      ),
-      confirmDismiss: (direction) async {
-        return await showDialog(
-          context: Get.context!,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Delete Item'),
-              content: Text('Are you sure you want to delete this item?'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.of(context).pop(false),
-                  child: Text('Cancel'),
-                ),
-                TextButton(
-                  onPressed: () => Navigator.of(context).pop(true),
-                  child: Text('Delete'),
-                ),
-              ],
-            );
-          },
-        );
-      },
-      onDismissed: (direction) {
-        categoryController.deleteCategory(catModel);
-      },
-      child: Card(
-        child: Row(
-          children: [
-            SizedBox(
-              height: 20.h,
-              width: 20.w, // Set the width equal to the height of the card
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(4.0), // Adjust the border radius as needed
-                  bottomLeft: Radius.circular(4.0), // Adjust the border radius as needed
-                ),
-                child: Image.network(
-                  imageUrl,
-                  fit: BoxFit.contain, // large as possible while still containing the source entirely within the target box.
+    return Card(
+      child: Stack(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                height: 20.h,
+                width: 20.w, // Set the width equal to the height of the card
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4.0), // Adjust the border radius as needed
+                    bottomLeft: Radius.circular(4.0), // Adjust the border radius as needed
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.contain, // large as possible while still containing the source entirely within the target box.
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: ListTile(
-                onTap:  () {
-                  categoryController.uidCurrent = catModel.uid;
-                  Get.to(() => UrlListUI());//Get.to(() => UrlListUI())  StoredListUI
-                },
-                title: Text(
-                  catModel.title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.sp,
-                  ),
-                ),
-                subtitle: Text(
-                  catModel.numItems.toString(),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.sp,
-                  ),
-                ),/*
-                trailing: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                    Get.to(UrlListUI());
+              Expanded(
+                child: ListTile(
+                  onTap: () {
+                    categoryController.uidCurrent = catModel.uid;
+                    Get.to(() => UrlListUI());
                   },
-                ),*/
+                  title: Text(
+                    catModel.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp,
+                    ),
+                  ),
+                  subtitle: Text(
+                    catModel.numItems.toString(),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () {
+                _showDeleteConfirmationDialog(catModel);
+              },
+              child: Icon(
+                Icons.delete,
+                color: Colors.black87,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
